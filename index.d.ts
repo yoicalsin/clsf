7c0242a6336eecc6a27b153468cfc1ef3e6f1457
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 11:29:17 -0500

    ci: 🎡 Main code was builded correctly !

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,5 @@
  * @description A function that merges given class names, no matter their format. Filters out invalid class names as well.
  * @param names any[]
  */
-export declare const classnamesFast: (...names: any[]) => any;
-export { classnamesFast as clsf };
-export default classnamesFast;
+export declare const clsf: (...names: any[]) => string;
+export default clsf;
--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const is_all_utils_1 = require("is-all-utils");
  * @description A function that merges given class names, no matter their format. Filters out invalid class names as well.
  * @param names any[]
  */
-exports.classnamesFast = (...names) => {
+exports.clsf = (...names) => {
     return names
         .reduce((classname, arg) => is_all_utils_1.isString(arg) || is_all_utils_1.isArray(arg) ? classname.concat(arg) : classname, [])
         .filter(Boolean)
         .join(' ');
 };
-exports.clsf = exports.classnamesFast;
-exports.default = exports.classnamesFast;
+exports.default = exports.clsf;
