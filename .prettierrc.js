c769d3c2c340b8ffd6531446d3a35e9b148eb0bd
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 10:45:12 -0500

    style: 💄 Aggregated more options !

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    trailingComma: 'all',
-    singleQuote: true,
-    tabWidth: 4,
-    semi: true,
+   trailingComma: 'all',
+   singleQuote: true,
+   tabWidth: 3,
+   semi: true,
+   arrowParens: 'always', // always | avoid
+   bracketSpacing: true,
 };
