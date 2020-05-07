ff4a430a769138621f11e35ec2ad4948fb37af11
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 10:58:32 -0500

    style: 💄 Build types for typescript !

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @description A function that merges given class names, no matter their format. Filters out invalid class names as well.
+ * @param names any[]
+ */
+export declare const classnamesFast: (...names: any[]) => any;
+export { classnamesFast as clsf };
+export default classnamesFast;
