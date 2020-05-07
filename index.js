075fa504e3b2ac382f72887babe868026ffe0491
Yoni Calsin <yonicb20@gmail.com>
2020-05-07 11:04:26 -0500

    refactor: 💡 Builded main code !

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const is_all_utils_1 = require("is-all-utils");
 /**
  * @description A function that merges given class names, no matter their format. Filters out invalid class names as well.
  * @param names any[]
  */
 exports.classnamesFast = (...names) => {
     return names
-        .reduce((classname, arg) => typeof arg === 'string' || arg instanceof Array
-        ? classname.concat(arg)
-        : classname, [])
+        .reduce((classname, arg) => is_all_utils_1.isString(arg) || is_all_utils_1.isArray(arg) ? classname.concat(arg) : classname, [])
         .filter(Boolean)
         .join(' ');
 };
